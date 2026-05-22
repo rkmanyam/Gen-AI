7720afce15096b011a118298ba5c504e4316a80c
Ram <ramdevopstraininggmail.onmicrosoft.com>
2026-05-22 08:31:16 +0530

    Renamed mcp streamable http and defined resources & prompts

---
+++ MCP-Server/library-mcp-resources/datastore.py
@@ -0,0 +1,110 @@
+BOOKS: dict[str, dict] = {
+    "B001": {
+        "Title": "The Great Gatsby",
+        "Author": "F. Scott Fitzgerald",
+        "Year": "1925",
+        "Is_Active": True,
+        "ISBN": "978-0743273565",
+        "Description": "A novel about the American dream and the decadence of the Jazz Age.",
+        "Genres": ["Classic", "Fiction"],
+        "Available_Copies": 5,
+        "Total_Copies": 10,
+        "Tags": ["Gatsby", "Classic", "American Literature", "Great"]
+    },
+    "B002": {
+        "Title": "To Kill a Mockingbird",
+        "Author": "Harper Lee",
+        "Year": "1960",
+        "Is_Active": True,
+        "ISBN": "978-0061120084",
+        "Description": "A novel about racial injustice in the Deep South, seen through the eyes of a young girl.",
+        "Genres": ["Classic", "Fiction"],
+        "Available_Copies": 5,
+        "Total_Copies": 10,
+        "Tags": ["Mockingbird", "Classic", "American Literature", "Racial Injustice"]
+    },
+    "B003": {
+        "Title": "1984",
+        "Author": "George Orwell",
+        "Year": "1948",
+        "Is_Active": True,
+        "ISBN": "978-0451524935",
+        "Description": "A dystopian social science fiction novel and cautionary tale about the dangers of totalitarianism.",
+        "Genres": ["Dystopian", "Fiction"],
+        "Available_Copies": 5,
+        "Total_Copies": 10,
+        "Tags": ["1984", "Dystopian", "American Literature", "Totalitarianism"]
+    },
+    "B004": {
+        "Title": "Pride and Prejudice",
+        "Author": "Jane Austen",
+        "Year": "1813",
+        "Is_Active": True,
+        "ISBN": "978-0141439518",
+        "Description": "A romantic novel of manners about Elizabeth Bennet and Mr. Darcy navigating love and social expectations.",
+        "Genres": ["Classic", "Romance"],
+        "Available_Copies": 8,
+        "Total_Copies": 12,
+        "Tags": ["Austen", "Romance", "Classic", "Social Commentary"]
+    }
+}
+
+
+STUDENTS: dict[str, dict] = {
+    "S001": {
+        "Name:": "Alice Johnson",
+        "Email": "alicej@mcp-university.edu",
+        "Phone": "555-1234",
+        "Enrolled_Courses": "GenAI",
+        "Age": 19,
+        "RollNumber": "13P31A04D1"
+   },
+   "S002": {
+        "Name:": "Bob Smith",
+        "Email": "bobs@mcp-university.edu",
+        "Phone": "555-5678",
+        "Enrolled_Courses": "GenAI",
+        "Age": 19,
+        "RollNumber": "13P31A04D2"
+   },
+   "S003": {
+        "Name:": "Charlie Brown",
+        "Email": "charlieb@mcp-university.edu",
+        "Phone": "555-9012",
+        "Enrolled_Courses": "Data Science",
+        "Age": 19,
+        "RollNumber": "13P31A04D3"
+   },
+   "S004": {
+        "Name:": "Diana Prince",
+        "Email": "dianap@university.edu",
+        "Phone": "555-3456",
+        "Enrolled_Courses": "Data Science",
+        "Age": 19,
+        "RollNumber": "13P31A04D4"
+   },
+   "S004": {
+        "Name:": "Charlie Brown",
+        "Email": "charlieb@mcp-university.edu",
+        "Phone": "555-9012",
+        "Enrolled_Courses": "Data Science",
+        "Age": 19,
+        "RollNumber": "13P31A04D5",
+   },
+   "S005": {
+        "Name:": "Charles Babbage",
+        "Email": "charlesb@mcp-university.edu",
+        "Phone": "555-9013",
+        "Enrolled_Courses": "Cloud Computing",
+        "Age": 19,
+        "RollNumber": "13P31A04D6",
+   },
+   "S006": {
+        "Name:": "Thukaramaraju",
+        "Email": "thukaramaraju@mcp-university.edu",
+        "Phone": "555-8231",
+        "Enrolled_Courses": "DevOps Engineering",
+        "Age": 19,
+        "RollNumber": "13P31A04D7",
+   }
+}
