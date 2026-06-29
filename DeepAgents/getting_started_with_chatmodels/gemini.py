fb9c2fb1bf32f3ff5f0d66756e4b00741b2580ba
Ram <ramdevopstraininggmail.onmicrosoft.com>
2026-06-29 20:24:15 +0530

    Started using chat models to get some response

---
+++ DeepAgents/getting_started_with_chatmodels/gemini.py
@@ -0,0 +1,13 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+interaction = client.interactions.create(
+    model="gemini-3.5-flash-lite",
+    input="What is the capital of France?"
+)
+
+print(interaction.output_text)
